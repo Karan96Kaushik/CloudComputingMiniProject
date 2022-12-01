b1dbeddd6b8e2a4e98ec26023a4d50464c6a5951
YupengLuo12 <119423252+YupengLuo12@users.noreply.github.com>
2022-12-01 11:45:05 +0000

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,8 +1,5 @@
 import requests
 
-#bike_url = "https://api.tfl.gov.uk/BikePoint/Search?query={keyword}"
-
-
 def search(query):
     url = f"https://api.tfl.gov.uk/BikePoint/Search?query={query}"
     return requests.get(url).json()
