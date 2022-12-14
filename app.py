b86cfe11dcaf266c2e846d0a5af7033a39e68f91
haoli9 <3292379021@qq.com>
2022-12-14 15:12:25 +0000

    any commit message

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 from helper import search
 from dotenv import dotenv_values
 from pymongo import MongoClient
+import hashlib
 
 config = dotenv_values(".env")
 
 def startup_db_client():
-	app.mongodb_client = MongoClient(config["ATLAS_URI"])
-	app.database = app.mongodb_client[config["DB_NAME"]]
-	print("Connected to the MongoDB database!")
-	app.database['Users'].insert_one({"name": "karan"})
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
 
 app = Flask(__name__)
 
@@ -22,26 +22,52 @@ def search_v1():
 	query = request.args.get('query')
 	return search(query)
 
-@app.route('/login')
+
+@app.route('/login',methods =['GET','POST'])
 def login():
-
 	if request.method == 'POST':
-		app.database['Users'].insert_one(request.form)
-		return True
-
+		email = request.form.get('email')
+		password = request.form.get('password')
+		user = app.databse['userinfo'].find_one({"username":email})
+		if user:
+			if encry(password) == user['password']:
+				session['username'] = user['username']
+				session['role'] = user['role']
+				print(session['role'])
+				if session.get('role') == 'admin':
+					return redirect('/admin')
+				return redirect('/')
+			else:
+				return redirect('/signup')
+		return render_template('login.html')
 	if request.method == 'GET':
 		return render_template('login.html')
 
+
+
 @app.route('/signup')
 def signup():
-
 	if request.method == 'POST':
-		data = request.form
-		app.database['Users'].insert_one(request.form)
-		return True
-
-	if request.method == 'GET':
+		email = request.form.get('email')
+		email_exist = app.database['user_info'].find_one({"user name":email})
+		if email_exist:
+			msg='email already existed!'
+			return render_template('signup.html',msg=msg)
+		else:
+			password = request.form.get('password')
+			password2 = input("please re-enter your password: ") #double check password
+			if password == password2:
+				msg = 'Signup success'
+				password = encry(password)
+				app.database['user_info'].insert_one({ "user name": email, "password": password,"role": "user" })
+			return render_template('signup.html')
+	else:
+			msg = 'please check your password'
+			return render_template('signup.html')
+if request.method == 'GET':
+	print('1')
 		return render_template('signup.html')
+
 
 
 if __name__ == '__main__':
